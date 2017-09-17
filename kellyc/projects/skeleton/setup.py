2d0d6b03107af22512e8a28564bb8e204d7b02bd
Kelly <colhtk@uw.edu>
2017-09-17 10:51:26 -0700

    adding project skeleton

---
+++ kellyc/projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'My Project',
+	'author': 'Kelly C.',
+	'url': 'URL to get at it.',
+	'download_rul': 'Where to download it.',
+	'author_email': 'audiblediner@fastmail.fm',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['NAME'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
