907a300b4dfaf99e196ba87c6968400548aa0e52
audiblediner <colhtk@uw.edu>
2017-12-12 21:20:04 -0800

    ex44

--- kellyc/ex44b.py
+++ kellyc/ex44b.py
@@ -1,19 +1,23 @@
 # Inheritance Versus Composition
-
+# Three ways parent and child classes interact 1) actions on the child imply an action on the parent 2) actions on the child override the action on the parent 3) actions on the child alter the action on the parent
 
 # This here is an example of overriding explicitly
+
+# create class parent that is a class object
 class Parent(object):
-    
-    def override(self):
-        print("PARENT override()")
-        
-class Child(Parent):
-    
+    # create override function to applies to self, print sumpin when invoked
     def override(self):
         print("PARENT override()")
 
+class Child(Parent):
+    # def override function that applies to self and print when invoked
+    def override(self):
+        print("CHILD override()")
+
+# assign variables to classes
 dad = Parent()
 son = Child()
 
+# run functions against what's been assigned to variables
 dad.override()
 son.override()
--- kellyc/ex44d.py
+++ kellyc/ex44d.py
@@ -2,32 +2,38 @@
 # err, this returns AttributeError: 'Child' object has no attribute 'implicit'
 # moving on for now
 
+# create class parent that inherits from class object
 class Parent(object):
-    
+
+    # define function override to apply to self, then print statement when invoked
     def override(self):
         print("PARENT override()")
-        
+
     def implicit(self):
         print("PARENT implicit()")
-        
+
     def altered(self):
         print("PARENT altered()")
-        
+
 class Child(object):
-    
+
     def override(self):
-        print("CHILD override()")        
-     
+        print("CHILD override()")
+
+    def implicit(self):
+        print("CHILD implicit()")
+
     def altered(self):
         print("CHILD, BEFORE PARENT altered()")
-        # call super with argument Child and self, then 
+        # call super with argument Child and self, then
         # call the function altered on what's returned
         super(Child, self).altered()
         print("CHILD, AFTER PARENT altered()")
-        
+
 dad = Parent()
 son = Child()
 
+# call the function implicit on whatever's assigned to variables
 dad.implicit()
 son.implicit()
 
